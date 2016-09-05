4eaba98cec3dad6c5a716d4f9e33204aec81756c
Stacie KITH <kith_s@etna-alternance.net>
2016-09-05 22:25:03 +0200

    env

--- app/env/production.php
+++ app/env/production.php
@@ -2,6 +2,7 @@
 
 putenv("APPLICATION_ENV=production");
 
-putenv("ZONEFLIGHT_DATABASE_HOST=127.0.0.1");
+putenv("ZONEFLIGHT_DATABASE_HOST=localhost");
 putenv("ZONEFLIGHT_DATABASE_NAME=zoneflight");
-putenv("ZONEFLIGHT_DATABASE_USER=root");
+putenv("ZONEFLIGHT_DATABASE_USER=zoneflight");
+putenv("ZONEFLIGHT_DATABASE_PWD=StacieJTM4ever<3");
